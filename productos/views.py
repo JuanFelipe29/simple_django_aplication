f70697720236125b73f908c3e399c898647485a3
Juan Felipe Alvear Estrada <juanalvear2009@gmail.com>
2023-09-01 20:42:43 -0500

    Template , vistas y adminDasboard

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponseGone
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
+
+# /productos
+
 def index(request):
-    return HttpResponseGone('Hola mundo')
+    productos = Producto.objects.all()
+    print(productos)
+    
+    return render(
+        request,
+        'index.html',
+        context={'productos': productos}
+    )
